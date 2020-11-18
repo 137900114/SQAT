e079ad4e22579dbdacb9fba2d44577fd285fd7ce
tiansuohaoer <137900114@qq.com>
2020-11-18 18:54:47 +0800

    finished Requirement3

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,18 @@ class Greeting{
             name = "my friend";
         }
 
+        boolean isAllUpperCase = true;
+        for(int i = 0;i != name.length();i++){
+            if(!Character.isUpperCase(name.charAt(i))){
+                isAllUpperCase = false;
+                break;
+            }
+        }
+
+        if(isAllUpperCase){
+            return "HELLO " + name + "!";
+        }
+
         return "Hello," +  name + ".";
     }
 }
--- test/GreetingTest.java
+++ test/GreetingTest.java
@@ -9,11 +9,14 @@ public class GreetingTest {
         Greeting test = new Greeting();
 
         assertEquals("Hello,Bob.",test.greeting("Bob"));
-        assertEquals("Hello,UDK.",test.greeting("UDK"));
-        assertEquals("Hello,FAQ.",test.greeting("FAQ"));
+        assertEquals("Hello,aUDK.",test.greeting("aUDK"));
+        assertEquals("Hello,aFAQ.",test.greeting("aFAQ"));
 
         //if the string pass into greeting is null
         assertEquals("Hello,my friend.",test.greeting(null));
 
+
+        assertEquals("HELLO JERRY!",test.greeting("JERRY"));
+        assertEquals("Hello,Dr.ASH.",test.greeting("Dr.ASH"));
     }
 }
